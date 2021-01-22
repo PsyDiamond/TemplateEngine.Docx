1ca5446349a928743bb86f1b513a5ddf0e9c2994
Михаил Малыхин <lextalionis1488@gmail.com>
2021-01-22 18:15:52 +0700

    Add FieldDeleteContent. This template need for delete field with parent paragraph

---
+++ sources/TemplateEngine.Docx/TemplateCustomContent/FieldDeleteContent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TemplateEngine.Docx
+{
+    public class FieldDeleteContent : HiddenContent<FieldDeleteContent>, IEquatable<FieldDeleteContent>
+    {
+        public FieldDeleteContent(string name)
+        {
+            Name = name;
+        }
+
+        public bool Equals(FieldDeleteContent other)
+        {
+            if (other == null) return false;
+
+            return Name.Equals(other.Name);
+        }
+
+        public override int GetHashCode()
+        {
+            return new { Name }.GetHashCode();
+        }
+
+        public override bool Equals(IContentItem other)
+        {
+            if (!(other is FieldDeleteContent)) return false;
+
+            return Equals((FieldDeleteContent)other);
+        }
+    }
+}
