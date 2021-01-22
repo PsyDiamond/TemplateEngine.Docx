ff80bcd52336ce37b8b5c99ba23ecdf6e69faf94
Михаил Малыхин <lextalionis1488@gmail.com>
2021-01-22 18:20:21 +0700

    Add TableDeleteContent. This template need for delete table

---
+++ sources/TemplateEngine.Docx/TemplateCustomContent/TableDeleteContent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TemplateEngine.Docx
+{
+    public class TableDeleteContent : HiddenContent<TableDeleteContent>, IEquatable<TableDeleteContent>
+    {
+        public TableDeleteContent(string name)
+        {
+            Name = name;
+        }
+
+        public override bool Equals(IContentItem other)
+        {
+            if (!(other is TableDeleteContent)) return false;
+
+            return Equals((TableDeleteContent)other);
+        }
+
+        public bool Equals(TableDeleteContent other)
+        {
+            if (other == null) return false;
+
+            return Name.Equals(other.Name);
+        }
+
+        public override int GetHashCode()
+        {
+            return new { Name }.GetHashCode();
+        }
+    }
+}
