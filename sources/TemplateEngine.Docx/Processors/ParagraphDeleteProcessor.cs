7b60a9d1416bbd54a32332f4543b88bb856b5ca5
Михаил Малыхин <lextalionis1488@gmail.com>
2021-01-22 18:30:52 +0700

    Add ParagraphDeleteProcessor for delete paragraphs from the end of the document, after all the actions

---
+++ sources/TemplateEngine.Docx/Processors/ParagraphDeleteProcessor.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+
+namespace TemplateEngine.Docx.Processors
+{
+    class ParagraphDeleteProcessor
+    {
+        public ProcessResult DeleteContent(XElement contentControl)
+        {
+            var processResult = ProcessResult.NotHandledResult;
+
+            var par = contentControl.Elements(W.p).LastOrDefault();
+            if (par != null)
+                par.Remove();
+
+            return processResult;
+        }
+    }
+}
